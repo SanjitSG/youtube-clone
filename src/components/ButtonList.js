58f98fa6b06bb40646c78cc0db1f4de97fe7cd42
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-05-08 01:03:31 +0530

    Button list

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,7 +1,33 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  const list = [
+    "All",
+    "Coding",
+    "News",
+    "Dance",
+    "Gym",
+    "Gaming",
+    "Racing",
+    "Cooking",
+    "Programming",
+    "Birds",
+    "Europe",
+    "Camera",
+    // "Boxing",
+    // "Chess",
+    // "Waterfall",
+    // "React",
+    // "Health",
+  ];
+  return (
+    <div className="flex">
+      {list.map((category) => (
+        <Button name={category} />
+      ))}
+    </div>
+  );
 };
 
 export default ButtonList;
