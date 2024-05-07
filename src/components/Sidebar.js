40a7625dfa689c1ce212a248bfb258c7cf8570ed
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-05-07 23:47:12 +0530

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { CgMediaLive } from "react-icons/cg";
+import { FaShoppingBag } from "react-icons/fa";
+import { IoMdMusicalNote } from "react-icons/io";
+import { LiaGripfire } from "react-icons/lia";
+import { MdHomeFilled, MdSubscriptions } from "react-icons/md";
+import { PiFilmSlateFill } from "react-icons/pi";
+import { SiYoutubegaming } from "react-icons/si";
 
 const Sidebar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="w-44 py-2 px-2 h-screen shadow-lg">
+      <ul>
+        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200 ">
+          <MdHomeFilled /> Home
+        </li>
+        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+          <MdSubscriptions /> Subscriptions
+        </li>
+      </ul>
+      <hr />
+      <h1 className="font-bold py-3">Explore</h1>
+      <ul>
+        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+          <LiaGripfire /> Trending
+        </li>
+        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+          <FaShoppingBag /> Shopping
+        </li>
+        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+          <IoMdMusicalNote /> Music
+        </li>
+        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+          <PiFilmSlateFill /> Films
+        </li>
+        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+          <CgMediaLive /> Live
+        </li>
+        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+          <SiYoutubegaming /> Gaming
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
