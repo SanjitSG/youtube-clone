cd95a98c153df7f7d4b97368b0153bb59ea1a5d1
Sanjit Gawade <sanjeet25.sg@gmail.com>
2024-05-08 00:00:00 +0530

    redux store

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,34 +9,34 @@ import { SiYoutubegaming } from "react-icons/si";
 
 const Sidebar = () => {
   return (
-    <div className="w-44 py-2 px-2 h-screen shadow-lg">
+    <div className="w-52 py-2 px-2 h-screen shadow-lg">
       <ul>
-        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200 ">
+        <li className="flex items-center justify-start gap-3 px-3 py-2 text-lg rounded-lg  cursor-pointer hover:bg-slate-200 ">
           <MdHomeFilled /> Home
         </li>
-        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+        <li className="flex items-center justify-start gap-3 px-3 py-2 text-lg rounded-lg cursor-pointer hover:bg-slate-200">
           <MdSubscriptions /> Subscriptions
         </li>
       </ul>
       <hr />
       <h1 className="font-bold py-3">Explore</h1>
       <ul>
-        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+        <li className="flex items-center justify-start gap-3 px-3 py-2 text-lg rounded-lg cursor-pointer hover:bg-slate-200">
           <LiaGripfire /> Trending
         </li>
-        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+        <li className="flex items-center justify-start gap-3 px-3 py-2 text-lg rounded-lg cursor-pointer hover:bg-slate-200">
           <FaShoppingBag /> Shopping
         </li>
-        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+        <li className="flex items-center justify-start gap-3 px-3 py-2 text-lg rounded-lg cursor-pointer hover:bg-slate-200">
           <IoMdMusicalNote /> Music
         </li>
-        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+        <li className="flex items-center justify-start gap-3 px-3 py-2 text-lg rounded-lg cursor-pointer hover:bg-slate-200">
           <PiFilmSlateFill /> Films
         </li>
-        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+        <li className="flex items-center justify-start gap-3 px-3 py-2 text-lg rounded-lg cursor-pointer hover:bg-slate-200">
           <CgMediaLive /> Live
         </li>
-        <li className="flex items-center justify-start gap-3 p-3 text-lg rounded-lg hover:bg-slate-200">
+        <li className="flex items-center justify-start gap-3 px-3 py-2 text-lg rounded-lg cursor-pointer hover:bg-slate-200">
           <SiYoutubegaming /> Gaming
         </li>
       </ul>
